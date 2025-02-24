4690e68b95bc65dae263272401132de234673d6b
jessupthefish <sgj1995@yahoo.com>
2025-02-24 13:57:24 -0800

    initial backend and server files

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json()); // Allows JSON request bodies
+app.use(cors()); // Enables Cross-Origin Resource Sharing
+
+// Simple Test Route
+app.get('/', (req, res) => {
+    res.send('IT Ticketing System Backend is Running');
+});
+
+// Start the Server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
